d7848e0a37e937e7824aac0622984d80938cf5ef
mohmtayham <mohmtayham@gmail.com>
2026-06-18 15:22:02 +0300

    dddsadsd

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -6,10 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  // Get current logged-in user's profile
+  @Get('me')
+  async getProfile(@Req() req: any) {
+    // TODO: Replace with req.user.id once your AuthGuard is set up
+    const userId = req.user?.id || '1'; 
+    return this.userService.findOne(userId);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Get('email/:email')
+  async findByEmail(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
+  }
 }
